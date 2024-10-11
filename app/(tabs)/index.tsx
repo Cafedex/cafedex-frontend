0b82276cb3a5b14cf7cb9884eac4088b308e208c
Hunter L. Melton <hunterlmelton@gmail.com>
2024-10-11 01:47:50 -0700

    bump from previous commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import React, { useMemo, useState } from 'react';
 import { Stack } from 'expo-router';
 import GuidesHeader from '@/components/GuidesHeader';
 import CafeMap from '@/components/CafeMap'
+import MapSideBanner from '@/components/MapSideBanner';
 
 const Page = () => {
   const [category, setCategory] = useState<string>('Pour-Over');
@@ -14,45 +15,67 @@ const Page = () => {
   const cafeData = {
     locations: [
       {
+        id: 0,
         name: "KAIZEN COFFEE",
         properties: {
           latitude: "37.545037038600306", 
           longitude:"-122.3060117391627",
-        }
+        },
       },
       {
+        id: 1,
         name: "Academic Coffee",
         properties: {
           latitude:"37.32918395915575",
           longitude:"-121.88407674457169"
-        }
+        },
       },
       {
+        id: 2,
         name:"The Coffee Movement",
         properties: {
           latitude: "37.77887282134126",
           longitude:"-122.47823596299988" 
-        }
+        },
       },
       {
+        id: 3,
         name: "Devout Coffee",
         properties: {
           latitude: "37.58005871563196",
-          longitude: "-121.96261316958278"
-        }
-      }
-
+          longitude:"-121.96261316958278"
+        },
+      },
+      {
+        id: 4,
+        name: "Coffee Water Lab",
+        properties: {
+          latitude: "37.315707529407284",
+          longitude:"-121.97650792383095"
+        },
+      },
+      {
+        id: 5,
+        name: "Saint Frank Coffee",
+        properties: {
+          latitude: "37.460032167347286",
+          longitude:"-122.18108435140324"
+        },
+      },
+      {
+        id: 6,
+        name: "AM Craft",
+        properties: {
+          latitude: "37.336553357787466",
+          longitude:"-121.87705831758508"
+        },
+      },
     ]
   }
 
   return (
     <View style={{ flex:1 }}>
-      <Stack.Screen 
-        options={{
-          header: () => <GuidesHeader onCategoryChanged={onDataChanged}/>
-          
-        }}
-      />
+ 
       <CafeMap cafes={cafeData} />
     </View>
   )
