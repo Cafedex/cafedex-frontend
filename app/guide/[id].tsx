7fd1e3b4c12ff210ba47be7b3a896e9f78536325
HLMelton <hunterlmelton@gmail.com>
2024-10-14 16:31:27 -0700

    guide page

---
+++ app/guide/[id].tsx
@@ -0,0 +1,56 @@
+import Colors from '@/constants/Colors';
+import { useOAuth } from '@clerk/clerk-expo';
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter, Stack, useLocalSearchParams } from 'expo-router';
+import { View, StyleSheet, TextInput, Text, TouchableOpacity} from 'react-native';
+import { defaultStyles } from '@/constants/Styles';
+import { useWarmUpBrowser } from '@/hooks/useWarmUpBrowser';
+
+const Page = () => {
+  const { id } = useLocalSearchParams();
+
+  return (
+    <View style={styles.container}>
+      <Text>Find a way to pass the data here for the cafe modal</Text>
+      <Text>Cafe ID: {id}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      padding: 26,
+    },
+  
+    seperatorView: {
+      flexDirection: 'row',
+      gap: 10,
+      alignItems: 'center',
+      marginVertical: 30,
+    },
+    seperator: {
+      fontFamily: 'mon-sb',
+      color: Colors.grey,
+      fontSize: 16,
+    },
+    btnOutline: {
+      backgroundColor: '#fff',
+      borderWidth: 1,
+      borderColor: Colors.grey,
+      height: 50,
+      borderRadius: 8,
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexDirection: 'row',
+      paddingHorizontal: 10,
+    },
+    btnOutlineText: {
+      color: '#000',
+      fontSize: 16,
+      fontFamily: 'mon-sb',
+    },
+  });
+  
+  export default Page;
